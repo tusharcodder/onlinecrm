8d0d7c0fbe490d74bb28af752a2716413d96ee96
Tushar0012 <tushar@openlogicsys.com>
2021-12-03 23:27:18 +0530

    Purchasse report issue

--- app/Http/Controllers/PurchaseReportController.php
+++ app/Http/Controllers/PurchaseReportController.php
@@ -48,7 +48,7 @@ class PurchaseReportController extends Controller
     public function index(Request $request)
     {
         //
-		
+		$purchaseorders = [];
 		$result = DB::table('purchase_orders')
 		->join('customer_orders','order_item_id','purchase_orders.isbn13')
 		->leftjoin('book_details','book_details.isbnno','purchase_orders.isbn13')
@@ -57,55 +57,53 @@ class PurchaseReportController extends Controller
 		)->groupby('purchase_orders.isbn13')->get();
 
 		if($result->count() > 0){
-				$purchaseorders = [];
-				foreach($result as $value){
-					if($value->quantity > 0){
-						$flag = 0;
-						$remainquantity = $value->quantity;
-						//check priority wise						
-						for($i=1; $i<10; $i++){
-							$venderdetails = DB::table('vendor_stocks')
-							->join('vendors','vendors.id','vendor_stocks.vendor_id')->select('vendors.name','author','publisher','quantity')
-							->where('vendors.priority',$i)->where('vendor_stocks.isbnno',$value->isbn13)
-							->get();
-							if($venderdetails->count() > 0){
-								//check vendor have quantity more then need
-								if($venderdetails[0]->quantity >= $remainquantity){
-									$flag = 1;
-									$dataarray = array(
-										"isbn13"=>$value->isbn13,
-										'book'=>$value->name,
-										'author'=>$venderdetails[0]->author,
-										'publisher'=>$venderdetails[0]->publisher,
-										'quantity'=>$remainquantity,
-										'vendor_name'=>$venderdetails[0]->name,
-									);
-									$purchaseorders[] =$dataarray;
-								}
-								else {
-									// get remain quantity and check in other vendor stock
-									$remainquantity = ($value->quantity - $venderdetails[0]->quantity); 
-									$dataarray = array(
-										"isbn13"=>$value->isbn13,
-										'book'=>$value->name,
-										'author'=>$venderdetails[0]->author,
-										'publisher'=>$venderdetails[0]->publisher,
-										'quantity'=> $venderdetails[0]->quantity,
-										'vendor_name'=>$venderdetails[0]->name,
-									);
-									$purchaseorders[] =$dataarray;
-								}
-									
+			foreach($result as $value){
+				if($value->quantity > 0){
+					$flag = 0;
+					$remainquantity = $value->quantity;
+					//check priority wise						
+					for($i=1; $i<10; $i++){
+						$venderdetails = DB::table('vendor_stocks')
+						->join('vendors','vendors.id','vendor_stocks.vendor_id')->select('vendors.name','author','publisher','quantity')
+						->where('vendors.priority',$i)->where('vendor_stocks.isbnno',$value->isbn13)
+						->get();
+						if($venderdetails->count() > 0){
+							//check vendor have quantity more then need
+							if($venderdetails[0]->quantity >= $remainquantity){
+								$flag = 1;
+								$dataarray = array(
+									"isbn13"=>$value->isbn13,
+									'book'=>$value->name,
+									'author'=>$venderdetails[0]->author,
+									'publisher'=>$venderdetails[0]->publisher,
+									'quantity'=>$remainquantity,
+									'vendor_name'=>$venderdetails[0]->name,
+								);
+								$purchaseorders[] =$dataarray;
 							}
-							if($flag > 0)
-									break;
-
+							else {
+								// get remain quantity and check in other vendor stock
+								$remainquantity = ($value->quantity - $venderdetails[0]->quantity); 
+								$dataarray = array(
+									"isbn13"=>$value->isbn13,
+									'book'=>$value->name,
+									'author'=>$venderdetails[0]->author,
+									'publisher'=>$venderdetails[0]->publisher,
+									'quantity'=> $venderdetails[0]->quantity,
+									'vendor_name'=>$venderdetails[0]->name,
+								);
+								$purchaseorders[] =$dataarray;
+							}
+								
 						}
-
+						if($flag > 0)
+							break;
 					}
-						
+
+				}
 					
-				}         
+				
+			}         
 		}
 		$purchaseorders = (new Collection($purchaseorders))->sortBy('book')->paginate(10)->setPath('');
 		return view('reports.purchasereport',compact('purchaseorders'))
